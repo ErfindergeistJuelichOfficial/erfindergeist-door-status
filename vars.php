1db6a271d81591d04dcb0141296e5033d2856025
Lars Eschweiler <lars.eschweiler@novacapta.com>
2025-05-25 16:45:38 +0200

    fix: vars

--- vars.php
+++ vars.php
@@ -8,10 +8,12 @@
   $_SESSION['egj_door_status_hidden_field_value_name'] = 'egj_door_status_value_hidden';
 
   $_SESSION['egj_door_status_token_input_name'] = 'egj_door_status_token_1';
-  $_SESSION['egj_door_status_token_input_2_name'] = 'egj_door_status_token_2';
-  $_SESSION['egj_door_status_token_input_3_name'] = 'egj_door_status_token_3';
+  $_SESSION['egj_door_status_token_input_name_2'] = 'egj_door_status_token_2';
+  $_SESSION['egj_door_status_token_input_name_3'] = 'egj_door_status_token_3';
 
-  $_SESSION['egj_door_status_option_name'] = 'egj_door_status_status';
+  $_SESSION['egj_door_status_option_name'] = 'egj_door_status_status_1';
+  $_SESSION['egj_door_status_option_name_2'] = 'egj_door_status_status_2';
+  $_SESSION['egj_door_status_option_name_3'] = 'egj_door_status_status_3';
   
 
 ?>
