7456378a19626d7f50ab6aa3e8d2eb54cf6ef90f
Lars Eschweiler <lars.eschweiler@novacapta.com>
2025-05-25 17:23:01 +0200

    fix: verify once

--- index.php
+++ index.php
@@ -25,24 +25,20 @@ require_once 'styles.php';
 function egj_door_status_settings_page() {
   $hash = "egj"; 
   
-  $token = "";
-  $token2 = "";
-  $token3 = "";
+  $token_val_1 = "";
+  $token_val_2 = "";
+  $token_val_3 = "";
 
-  ?>
-  <p>isset: <?php echo isset($_POST[ $_SESSION['egj_door_status_hidden_field_input_name'] ]) ? "yes": "no" ?></p>
-  <p>hash is eq: <?php echo  $_POST[ $_SESSION['egj_door_status_hidden_field_input_name'] ] === $hash ? "yes": "no" ?></p>
-
-  <?php
-  // Check if the user has submitted the form
-  if( isset($_POST[ $_SESSION['egj_door_status_hidden_field_input_name'] ]) && $_POST[ $_SESSION['egj_door_status_hidden_field_input_name'] ] === $hash ) {
-    $token = $_POST[ $_SESSION['egj_door_status_token_input_name'] ];
-    $token2 = $_POST[ $_SESSION['egj_door_status_token_input_name_2'] ];
-    $token3 = $_POST[ $_SESSION['egj_door_status_token_input_name_3'] ];
+    // Check if the user has submitted the form
+  // if( isset($_POST[ $_SESSION['egj_door_status_hidden_field_input_name'] ]) && $_POST[ $_SESSION['egj_door_status_hidden_field_input_name'] ] === $hash ) {
+  if ( empty($_POST) || !wp_verify_nonce($_POST['egj_door_status_field'],'egj_door_status_action') ) {
+    $token_val_1 = $_POST[ $_SESSION['egj_door_status_token_input_name'] ];
+    $token_val_2 = $_POST[ $_SESSION['egj_door_status_token_input_name_2'] ];
+    $token_val_3 = $_POST[ $_SESSION['egj_door_status_token_input_name_3'] ];
    
-    update_option( $_SESSION['egj_door_status_token_option_name'], $token );
-    update_option( $_SESSION['egj_door_status_token_option_name_2'], $token2 );
-    update_option( $_SESSION['egj_door_status_token_option_name_3'], $token3 );
+    update_option( $_SESSION['egj_door_status_token_option_name'], $token_val_1 );
+    update_option( $_SESSION['egj_door_status_token_option_name_2'], $token_val_2 );
+    update_option( $_SESSION['egj_door_status_token_option_name_3'], $token_val_3 );
    
     // Put a "settings saved" message on the screen
     ?>
@@ -51,9 +47,9 @@ function egj_door_status_settings_page() {
   }
   else {
     // If the form hasn't been submitted, get the option value
-    $token = get_option( $_SESSION['egj_door_status_token_option_name'] );
-    $token2 = get_option( $_SESSION['egj_door_status_token_option_name_2'] );
-    $token3 = get_option( $_SESSION['egj_door_status_token_option_name_3'] );
+    $token_val_1 = get_option( $_SESSION['egj_door_status_token_option_name'] );
+    $token_val_2 = get_option( $_SESSION['egj_door_status_token_option_name_2'] );
+    $token_val_3 = get_option( $_SESSION['egj_door_status_token_option_name_3'] );
   }
 
   // Form
@@ -61,14 +57,15 @@ function egj_door_status_settings_page() {
     <div>
       <h3>Erfindergeist Door Status Settings</h3>
       <form name="form1" method="post" action="">
-        <input type="hidden" name="<?php echo $_SESSION['egj_door_status_hidden_field_input_name']; ?>" value="<?php echo esc_attr($hash) ?>">
+        <!-- <input type="hidden" name="<?php echo $_SESSION['egj_door_status_hidden_field_input_name']; ?>" value="<?php echo esc_attr($hash) ?>"> -->
+        <?php wp_nonce_field('egj_door_status_action','egj_door_status_field'); ?>
 
         <label for="token1">Token 1</label><br>
-        <input id="token1" type="text" name="<?php echo $_SESSION['egj_door_status_token_input_name']; ?>" value="<?php echo isset($token) ? esc_attr($token) : ''; ?>"><br>
+        <input id="token1" type="text" name="<?php echo $_SESSION['egj_door_status_token_input_name']; ?>" value="<?php echo isset($token_val_1) ? esc_attr($token_val_1) : ''; ?>"><br>
         <label for="token2">Token 2</label><br>
-        <input id="token2" type="text" name="<?php echo $_SESSION['egj_door_status_token_input_name_2']; ?>" value="<?php echo isset($token2) ? esc_attr($token2) : ''; ?>"><br>
+        <input id="token2" type="text" name="<?php echo $_SESSION['egj_door_status_token_input_name_2']; ?>" value="<?php echo isset($token_val_2) ? esc_attr($token_val_2) : ''; ?>"><br>
         <label for="token3">Token 3</label><br>
-        <input id="token3" type="text" name="<?php echo $_SESSION['egj_door_status_token_input_name_3']; ?>" value="<?php echo isset($token3) ? esc_attr($token3) : ''; ?>"><br>
+        <input id="token3" type="text" name="<?php echo $_SESSION['egj_door_status_token_input_name_3']; ?>" value="<?php echo isset($token_val_3) ? esc_attr($token_val_3) : ''; ?>"><br>
         <p class="submit">
           <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
         </p>
