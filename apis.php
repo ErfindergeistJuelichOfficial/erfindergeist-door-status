7e7af11505b52f014f450bbf640b007989956a55
Lars Eschweiler <lars.eschweiler@novacapta.com>
2025-06-09 14:47:51 +0200

    fix: api data body

--- apis.php
+++ apis.php
@@ -89,7 +89,8 @@ function egj_door_status_post_api( WP_REST_Request $request){
 
   // $jsonString = file_get_contents('php://input');
   // $jsonData = $request->get_json_params();
-  $body = $request->get_body();
+  // $body = $request->get_body();
+  
   $data = json_decode(file_get_contents('php://input'), true);
 
   $data['dateTime'] = (new DateTime())->format(DateTime::ATOM);
@@ -101,7 +102,7 @@ function egj_door_status_post_api( WP_REST_Request $request){
 
   // $state = json_string_to_room_state($jsonData);
     // $jsonString = json_encode($jsonData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)
-    update_option( $_SESSION['egj_door_status_option_name'], $body);
+    update_option( $_SESSION['egj_door_status_option_name'], $data );
     $response = new WP_REST_Response($data );
     $response->set_status(200);
 
